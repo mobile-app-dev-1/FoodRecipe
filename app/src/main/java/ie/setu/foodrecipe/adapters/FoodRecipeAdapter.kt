68f7cff9bcd50d6d236dbe63988b415df86044a3
Zaradin <crottyjj@hotmail.com>
2024-03-14 15:58:25 +0000

    formatting and comments

--- app/src/main/java/ie/setu/foodrecipe/adapters/FoodRecipeAdapter.kt
+++ app/src/main/java/ie/setu/foodrecipe/adapters/FoodRecipeAdapter.kt
@@ -2,18 +2,23 @@ package ie.setu.foodrecipe.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import ie.setu.foodrecipe.databinding.CardFoodrecipeBinding
 import ie.setu.foodrecipe.models.RecipeModel
 
-
 interface FoodRecipeListener {
     fun onFoodRecipeClick(foodrecipe: RecipeModel)
 }
 
-class FoodRecipeAdapter constructor(private var recipes: List<RecipeModel>, private val listener: FoodRecipeListener) :
-    RecyclerView.Adapter<FoodRecipeAdapter.MainHolder>() {
+class FoodRecipeAdapter constructor(
+    private var recipes: List<RecipeModel>,
+    private val listener: FoodRecipeListener
+) : RecyclerView.Adapter<FoodRecipeAdapter.MainHolder>(), Filterable {
+
+    private var filteredRecipes: List<RecipeModel> = recipes
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
         val binding = CardFoodrecipeBinding
@@ -23,13 +28,13 @@ class FoodRecipeAdapter constructor(private var recipes: List<RecipeModel>, priv
     }
 
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val recipe = recipes[holder.adapterPosition]
+        val recipe = filteredRecipes[position]
         holder.bind(recipe, listener)
     }
 
-    override fun getItemCount(): Int = recipes.size
+    override fun getItemCount(): Int = filteredRecipes.size
 
-    class MainHolder(private val binding : CardFoodrecipeBinding) :
+    class MainHolder(private val binding: CardFoodrecipeBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(recipe: RecipeModel, listener: FoodRecipeListener) {
@@ -40,7 +45,35 @@ class FoodRecipeAdapter constructor(private var recipes: List<RecipeModel>, priv
             Picasso.get()
                 .load(recipe.image)
                 .into(binding.recipeImage)
-            binding.root.setOnClickListener { listener.onFoodRecipeClick(recipe)}
+            binding.root.setOnClickListener { listener.onFoodRecipeClick(recipe) }
+        }
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filteredList = mutableListOf<RecipeModel>()
+                val query = constraint?.toString()?.trim()?.lowercase()
+                if (query.isNullOrEmpty()) {
+                    filteredList.addAll(recipes)
+                } else {
+                    recipes.forEach { recipe ->
+                        // check if search query is contained in recipes -> title/description/cuisine
+                        if (recipe.title.lowercase().contains(query) || recipe.description.lowercase().contains(query) || recipe.cuisine.lowercase().contains(query)
+                        ) {
+                            filteredList.add(recipe)
+                        }
+                    }
+                }
+                val results = FilterResults()
+                results.values = filteredList
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                filteredRecipes = results?.values as? List<RecipeModel> ?: emptyList()
+                notifyDataSetChanged()
+            }
         }
     }
 }
