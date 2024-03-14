d1e06dac8ad2043cc3c82434cf75d3847636c637
Zaradin <crottyjj@hotmail.com>
2024-03-14 18:59:37 +0000

    kdoc comments

--- app/src/main/java/ie/setu/foodrecipe/models/FoodRecipeMemStore.kt
+++ app/src/main/java/ie/setu/foodrecipe/models/FoodRecipeMemStore.kt
@@ -2,22 +2,52 @@ package ie.setu.foodrecipe.models
 
 import timber.log.Timber.i
 
+/**
+ * A singleton object to generate unique IDs for recipe objects.
+ */
 var lastId = 0L
+
+/**
+ * Retrieves the next available ID and increments the [lastId] by 1.
+ *
+ * @return The next available ID.
+ */
 internal fun getId() = lastId++
 
-class FoodRecipeMemStore : FoodRecipeStore{
+/**
+ * An in-memory implementation of [FoodRecipeStore] interface to store recipe data.
+ */
+class FoodRecipeMemStore : FoodRecipeStore {
+    /**
+     * A list to store recipe objects.
+     */
     val recipes = ArrayList<RecipeModel>()
 
+    /**
+     * Retrieves all recipes stored in this memory store.
+     *
+     * @return A list of all recipes.
+     */
     override fun findAll(): List<RecipeModel> {
         return recipes
         logAll()
     }
 
+    /**
+     * Adds a new recipe to the memory store.
+     *
+     * @param recipe The recipe to be added.
+     */
     override fun create(recipe: RecipeModel) {
         recipe.id = getId()
         recipes.add(recipe)
     }
 
+    /**
+     * Updates an existing recipe in the memory store.
+     *
+     * @param recipe The updated recipe.
+     */
     override fun update(recipe: RecipeModel) {
         val foundRecipe: RecipeModel? = recipes.find { p -> p.id == recipe.id }
         if (foundRecipe != null) {
@@ -31,6 +61,11 @@ class FoodRecipeMemStore : FoodRecipeStore{
         }
     }
 
+    /**
+     * Deletes a recipe from the memory store by its ID.
+     *
+     * @param id The ID of the recipe to be deleted.
+     */
     override fun deleteById(id: Long) {
         val iterator = recipes.iterator()
         while (iterator.hasNext()) {
@@ -42,7 +77,10 @@ class FoodRecipeMemStore : FoodRecipeStore{
         }
     }
 
+    /**
+     * Logs all recipes stored in the memory store.
+     */
     fun logAll() {
-        recipes.forEach{ i("${it}") }
+        recipes.forEach { i("${it}") }
     }
 }
